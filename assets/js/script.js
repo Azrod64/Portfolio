e377be5800694d5c558b320d1774c736187effd0
Romuald Moisan <moisan.romuald@mail.com>
2024-02-02 16:38:21 +0100

    Ajout de Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+const Http = new XMLHttpRequest();
+const url='https://ade-web-consult.univ-amu.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?projectId=8&resources=3929&calType=ical&firstDate=2024-01-29&lastDate=2024-02-02';
+Http.open("POST", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
 }
+
