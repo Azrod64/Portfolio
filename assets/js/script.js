d38f4ffa80d38f5f51004666f2f50af19ce3dee5
Romuald Moisan <moisan.romuald@mail.com>
2024-05-06 18:18:30 +0200

    Ajouyr de la page Projet

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const Http = new XMLHttpRequest();
-const url='https://ade-web-consult.univ-amu.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?projectId=8&resources=3929&calType=ical&firstDate=2024-01-29&lastDate=2024-02-02';
-Http.open("POST", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
-
