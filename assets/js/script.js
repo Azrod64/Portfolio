c9ff2597985b088c64b1dc7ac1e3d48ac272dbff
Romuald Moisan <moisan.romuald@gmail.com>
2024-06-02 19:29:29 +0200

    Set to dark by default

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,14 @@
 
 const $themeBtn = document.querySelector("[data-theme-btn]");
 const $HTML = document.documentElement;
-let isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
+let isDark = true; //window.matchMedia("(prefers-color-scheme:dark)").matches;
 
 if(sessionStorage.getItem("theme")) {
     $HTML.dataset.theme = sessionStorage.getItem("theme");
 } else {
-    $HTML.dataset.theme = isDark ? "dark" : "light";
+    $HTML.dataset.theme = "dark";
+    sessionStorage.setItem("theme", "dark");
+    // $HTML.dataset.theme = isDark ? "dark" : "light";
 }
 
 const changeTheme = () => {
