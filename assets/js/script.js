e1a6e9686764ae6978087b4995f28cfa1fe1666c
Romuald Moisan <moisan.romuald@gmail.com>
2024-06-02 19:31:16 +0200

    Rechange it

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,12 @@
 
 const $themeBtn = document.querySelector("[data-theme-btn]");
 const $HTML = document.documentElement;
-let isDark = true; //window.matchMedia("(prefers-color-scheme:dark)").matches;
+let isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
 if(sessionStorage.getItem("theme")) {
     $HTML.dataset.theme = sessionStorage.getItem("theme");
 } else {
-    $HTML.dataset.theme = "dark";
-    sessionStorage.setItem("theme", "dark");
-    // $HTML.dataset.theme = isDark ? "dark" : "light";
+    $HTML.dataset.theme = isDark ? "dark" : "light";
 }
 
 const changeTheme = () => {
